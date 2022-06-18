cb19401ecf262d095004fb1b329a966c05b56d11
Mikholae Baniaga <mikholae.baniaga@gmail.com>
2022-06-18 19:29:59 +0800

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 
 // Initialize Routes
 const apiRoute = require('./src/routes/api.route')
+const res = require('express/lib/response')
 
 require('dotenv').config()
 const port = process.env.HPORT
@@ -19,7 +20,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 // API
-app.use('/api/', apiRoute)
+//app.use('/api/', apiRoute)
+app.get('/', (req,res)=>{
+    res.json({sucess: "TANG INA MO"})
+})
 
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`)
