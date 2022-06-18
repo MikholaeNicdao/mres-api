ace7ed5065382a0e3d6de0544364fc7e77618a1f
Mikholae Baniaga <mikholae.baniaga@gmail.com>
2022-06-18 20:30:29 +0800

    updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 
 // Initialize Routes
-const apiRoute = require('./src/routes/api.route')
+const apiRoute = require('./sample')
 
 require('dotenv').config()
 const port = process.env.PORT || 8080
---
+++ sample.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/home', (req,res)=>{
+    res.json({SAMPLE: "AHHAHAAHH"})
+})
+
+module.exports = router
